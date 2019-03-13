aa870efb8119a15938e9d31bac47b1a9f2fe0464
Mariah Alderson <mariah@Mariahs-MBP.fanshawec.ca>
2019-03-13 13:38:49 -0400

    data routing test

---
+++ data.js
@@ -0,0 +1,18 @@
+const data = {
+  
+  name: "Mariah",
+  role: "developer",
+
+  user1:{
+    name: "bob",
+    role: "chef"
+  },
+
+  user2:{
+    name: "Mary",
+    role: "food tester"
+  }
+
+};
+
+module.exports = data;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+const data = require(`../data`);
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+// ':' is dynamic placeholder
+router.get('/:user', function(req, res, next) {
+  //res.render('index', { title: 'Express' });
+  console.log('data: ',data);
+  //req.params is placeholder
+  console.log('user: ',req.params.user);
+  res.render('userpage', data[req.params.user]);
 });
 
 module.exports = router;
