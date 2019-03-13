ebcd541f4111556ddc737a0daddf0f9c015655ef
Mariah Alderson <mariah@Mariahs-MBP.fanshawec.ca>
2019-03-13 13:23:49 -0400

    style test

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+//express stores web files here
 app.use(express.static(path.join(__dirname, 'public')));
 
 //set port
