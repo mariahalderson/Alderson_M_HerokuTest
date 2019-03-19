9f221acf09d4e91f6f04b5f54285fbe75ee67b53
Mariah Alderson <mariah@Mariahs-MacBook-Pro.local>
2019-03-19 19:35:30 -0400

    passing data through views

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,10 +1,10 @@
-var obj = document.querySelector("object");
-var svg = obj.contentDocument;
-var circle = svg.querySelectorAll("g");
-//console.log("hello");
-
-//  console.log(svg);
-circle.forEach(circ=>circ.classList.add("animation"));
+// var obj = document.querySelector("object");
+// var svg = obj.contentDocument;
+// var circle = svg.querySelectorAll("g");
+// //console.log("hello");
+//
+// //  console.log(svg);
+// circle.forEach(circ=>circ.classList.add("animation"));
 
 //images for bomb Beans
 var tiles = document.querySelectorAll(".tile");
--- routes/index.js
+++ routes/index.js
@@ -10,12 +10,18 @@ router.get('/', function(req, res, next) {
 });
 
 // ':' is dynamic placeholder
-router.get('/:user', function(req, res, next) {
-  //res.render('index', { title: 'Express' });
-  console.log('data: ',data);
-  //req.params is placeholder
-  console.log('user: ',req.params.user);
-  res.render('userpage', data[req.params.user]);
+// router.get('/:user', function(req, res, next) {
+//   //res.render('index', { title: 'Express' });
+//   console.log('data: ',data);
+//   //req.params is placeholder
+//   console.log('user: ',req.params.user);
+//   res.render('userpage', data[req.params.user]);
+// });
+
+router.get('/beans/:beantype', function(req, res, next) {
+  //console.log('data: ',data);
+  console.log('beans: ',req.params.beantype);
+  res.render('beanspage', data.beans[req.params.beantype]);
 });
 
 module.exports = router;
